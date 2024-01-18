630f861d8ee3af6d6773009ec67568f4538511d1
Soumyendu <cyberrrnaut@gmail.com>
2024-01-19 01:10:44 +0530

    Connected backend with MongoDb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+ mongoose
+.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log("Mongo DB connected!!");
+})
+.catch((error)=>{
+    console.log("Database Error: ",error);
+})
+
+ 
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("Port is listening on port 3000");
-})
+app.listen(3000, () => {
+  console.log("Port is listening on port 3000");
+});
