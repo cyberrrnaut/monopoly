4400d3fc4340f86cc38c47d006ce2a62cfaf18d6
Soumyendu <cyberrrnaut@gmail.com>
2024-01-23 00:48:13 +0530

    Created Sign in page with functionalities

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,19 +1,17 @@
-import React, { useState } from "react";
+import react, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-function SignUp() {
+export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const navigate= useNavigate();
+  const navigate = useNavigate();
   const handleChange = (e) => {
-   
     setFormData({
-      ...formData, //using spread operator to keep previous values
+      ...formData,
       [e.target.id]: e.target.value,
     });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -40,7 +38,6 @@ function SignUp() {
       setError(error.message);
     }
   };
-  // console.log(formData);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
@@ -84,4 +81,3 @@ function SignUp() {
     </div>
   );
 }
-export default SignUp;
