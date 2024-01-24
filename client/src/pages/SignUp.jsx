7be876bcf96fe93dc6e6385ba1b1606adbdb6858
Soumyendu <cyberrrnaut@gmail.com>
2024-01-25 02:10:01 +0530

    fixed the bug in sign in with google

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,12 +1,12 @@
-import react, { useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -71,6 +71,7 @@ export default function SignUp() {
         >
           {loading ? "Loading..." : "Sign Up"}
         </button>
+        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
