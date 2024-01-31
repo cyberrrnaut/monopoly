e792f51261f8909aabf465c0c8488a7e76bf0390
Soumyendu <cyberrrnaut@gmail.com>
2024-02-01 02:43:07 +0530

    Created Listing Page UI and Functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Profile from './pages/Profile'
-import Home from './pages/Home'
-import About from './pages/About'
-import SignUp from './pages/SignUp'
-import Signin from './pages/Signin'
-import Header from './components/Header'
-import PrivateRoute from './components/PrivateRoute'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
 
 function App() {
-  
   return (
-  
-   <BrowserRouter>
-   <Header/>
-   <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/sign-in' element={<Signin/>}/>
-    <Route path='/sign-up' element={<SignUp/>}/>
-    <Route path='/about' element={<About/>}/>
-    <Route element={<PrivateRoute />}>
-    <Route path='/profile' element={<Profile/>}/>
-    </Route>
-   </Routes>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
